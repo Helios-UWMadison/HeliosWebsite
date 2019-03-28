68b7d6c34d36a3d6136b577666d076585e7e00c7
Grant <grantmbb@live.com>
2019-03-27 21:20:34 -0500

    Edit nav and footer

--- js/quote.js
+++ js/quote.js
@@ -3,12 +3,15 @@ $(function(){
   var quoteSpace = $("#quote");
   var quotes = new Array(10);
 
-  console.log(quoteSpace.html());
-
   for(var i = 0; i < 10; i++){
     quotes[i] = "Hello there " + i;
   }
 
   quoteSpace.html(quotes[Math.floor(Math.random()*10)]);
+
+
+  var quoteSpace = $("#blog");
+
+  quoteSpace.html("New Blog Post is: Basic Blog posted on January 2018");
 
 });
