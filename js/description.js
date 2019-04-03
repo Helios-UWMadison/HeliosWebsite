33a26b99976ca57ab4486a797160cad185866771
Grant <grantmbb@live.com>
2019-04-02 22:14:47 -0500

    Added more to panels, started on energy team panel

--- js/description.js
+++ js/description.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
-  $(".flex-item").click(function(option){
-    var buttonPressed = ( $( this ).text());
+$(document).ready(function() {
+  $(".flex-item").click(function(option) {
+    var buttonPressed = ($(this).text());
 
-    $(".flex-item").each(function(index){
-      $( this ).removeClass("active");
+    $(".flex-item").each(function(index) {
+      $(this).removeClass("active");
     })
 
-    $( this ).addClass("active");
+    $(this).addClass("active");
 
-    $(".panel").each(function(index){
-       if(buttonPressed != $( this ).text().trim()){
-       $( this ).css("display", "none");
-     }else{
-       $( this ).css("display", "");
+    $(".panel").each(function(index) {
 
-     }
+      var firstWordEnd = $(this).text().trim().indexOf(" ");
+      var firstWord = $(this).text().trim().substring("0", firstWordEnd);
+
+      if (buttonPressed.trim() !== (firstWord.trim())) {
+        $(this).css("display", "none");
+      } else {
+        $(this).css("display", "");
+
+      }
     })
 
   });
